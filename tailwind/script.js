52cee761cac66d944e22b02bdd8df6be287235a0
Abhimanyu <abhimanyupayasi01@gmail.com>
2024-04-06 20:42:21 +0530

    tailwind

---
+++ tailwind/script.js
@@ -0,0 +1,10 @@
+// gsap.to(".box", { rotation: 360, x: 10, duration: 1 });
+
+// gsap.from(".box", { rotation: 360, x: 10, duration: 3 })
+
+// gsap.to(".box", { rotation: 360, x: 10, duration: 3, delay: 2 })
+
+document.querySelector('.link').addEventListener('click', function () {
+    window.location.href = "./contact.html";
+}
+);
